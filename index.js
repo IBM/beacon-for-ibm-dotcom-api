89e5f5ac10cc5d2c5259b0706c254bff2ef96b94
Kenny Lam <kennylam@protonmail.com>
2021-11-30 14:31:03 -0500

    fix(cors): use cors for all routes

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 
 const port = process.env.PORT || argv.port || 8080;
 
-app.get('/', cors(), async(req, res) => {
+app.use(cors());
+
+app.get('/', async(req, res) => {
   // get Beacon params
   const params = req.query;
   params.raw = true;
